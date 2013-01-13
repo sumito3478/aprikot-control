1fbf37334066834ebd9132f0226e63c04b2e05e3
sumito3478 <sumito3478@gmail.com>
2013-01-13 21:52:16 +0900

    Use aprikot-sbt plugin for project settings

--- project/Build.scala
+++ project/Build.scala
@@ -1,46 +1,17 @@
 import sbt._
 import Keys._
 
+import info.sumito3478.aprikot.sbt._
+
 object AprikotControlBuild extends Build {
 
   lazy val project = Project(
     id = "aprikot-control",
     base = file(".")
+  ).settings(StandardProject.newSettings :_*
   ).settings(
     Seq(
-      version := "0.0.4-SNAPSHOT",
-      scalaVersion := "2.10.0",
-      autoCompilerPlugins := true,
-      addCompilerPlugin("org.scala-lang.plugins" % "continuations" % "2.10.0"),
-      scalacOptions ++= Seq(
-        "-P:continuations:enable",
-        "-target:jvm-1.6",
-        "-deprecation",
-        "-feature",
-        "-explaintypes",
-        "-unchecked"
-        ),
-      publishMavenStyle := true,
-      publishTo := Some(Resolver.sftp(
-        "sumito3478 Maven Repository (push)",
-        "maven.sumito3478.info",
-        46877,
-        "/var/www/maven.sumito3478.info") as (
-        "sumito3478",
-        new java.io.File(
-          new java.io.File(System.getProperty("user.home")),
-          ".ssh/sumito3478-sshkey"))),
-      resolvers := Seq(
-        "sumito3478 Maven Repository (pull)" at "http://maven.sumito3478.info/",
-        "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots",
-        "Maven Repository Mirror" at "http://uk.maven.org/maven2"),
-      externalResolvers <<= resolvers map {
-        rs =>
-          Resolver.withDefaultResolvers(rs, mavenCentral = false)
-      },
-      libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest" % "2.0.+" % "test"
-      )
+      version := "0.0.4-SNAPSHOT"
     ): _*
   )
 
