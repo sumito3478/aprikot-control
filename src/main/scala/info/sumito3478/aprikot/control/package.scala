a44adb562dd92dcf927665f6336b2d76efec7235
sumito3478 <sumito3478@gmail.com>
2013-01-13 09:09:06 +0900

    Add doc comments for pipe operators

--- src/main/scala/info/sumito3478/aprikot/control/package.scala
+++ src/main/scala/info/sumito3478/aprikot/control/package.scala
@@ -17,18 +17,68 @@
 package info.sumito3478.aprikot
 
 package object control {
+  /**
+   * Pimp-my-library class to add |> operator.
+   *
+   * |> operator passes the result of the left side to the function on the right
+   * side (forward pipe operator).
+   *
+   * Example usage:
+   * {{{
+   * import info.sumito3478.aprikot.control.ForwardPipeOperator
+   *
+   * val i = -1 |> math.abs
+   * // i == 1
+   *
+   * def function1(x: Int) = x + 1
+   *
+   * def function2(x : Int) = x * 2
+   *
+   * val result = 100 |> function1 |> function2
+   * // result == 202
+   * }}}
+   */
   implicit class ForwardPipeOperator[A](underlined: A) {
     def |>[R](f: A => R): R = {
       f(underlined)
     }
   }
 
+  /**
+   * Pimp-my-library class to add ||> operator.
+   *
+   * ||> operator passes the tuple of two arguments on the left side to the
+   * function on the right side.
+   *
+   * Example usage:
+   * {{{
+   * import info.sumito3478.aprikot.control.ForwardPipeOperatorOfTuple2
+   *
+   * val i = (2.0, 2.0) ||> math.pow
+   * // i == 4.0
+   * }}}
+   */
   implicit class ForwardPipeOperatorOfTuple2[A, B](underlined: (A, B)) {
     def ||>[R](f: (A, B) => R): R = {
       f(underlined._1, underlined._2)
     }
   }
 
+  /**
+   * Pimp-my-library class to add |||> operator.
+   *
+   * ||> operator passes the tuple of three arguments on the left side to the
+   * function on the right side.
+   *
+   * Example usage:
+   * {{{
+   * import info.sumito3478.aprikot.control.ForwardPipeOperatorOfTuple3
+   *
+   * def max(x: Int, y: Int, z: Int): Int = math.max(x, math.max(y, z))
+   * val i = (3, 1, 2) |||> max
+   * // i == 3
+   * }}}
+   */
   implicit class ForwardPipeOperatorOfTuple3[A, B, C](underlined: (A, B, C)) {
     def |||>[R](f: (A, B, C) => R): R = {
       f(underlined._1, underlined._2, underlined._3)
